426bc8a2a124e5d60a642d3d192eb954fa55eafb
Joel Pierre <joel.pierre-powell@foolproof.co.uk>
2019-01-28 11:24:58 +0000

    Add words.js

--- static/words.js
+++ static/words.js
@@ -1,47 +1,49 @@
-const words = [
-
-  {
-    word: "word",
-    pronounced: "wurd",
-    origin: "origin",
-    word_type: "noun|verb|etc",
-    synonyms: [
-      // A word or phrase that means exactly or nearly the same as another word or phrase in the same language,
-      // for example shut is a synonym of close.
-      // {word object}
-    ],
-    category: {
-      id: 1,
-      title: "Category",
-      slug: "category",
-      words: [
-        // {...}
-      ],
-    },
-    tags: [
-      {
-        id: 1,
-        title: "Tag[n]",
-        slug: "tag[n]",
-      },
-    ],
-    alternatives: [
-      "text",
-      "copy",
-    ],
-    examples: [
-      {
-        type: "code-block",
-        code: "<code></code>",
-      },
-      {
-        type: "codepen",
-        link: "url here"
-      }
+const words = {
+  word: "example",
+  origin: "origin",
+  syllables: {
+    count: 3,
+    list: [
+      "ex",
+      "am",
+      "ple",
     ],
   },
-
-];
+  pronunciation: {
+    all: "ɪɡ'zæmpəl",
+  },
+  results: [
+    {
+      definition: "",
+      partOfSpeech: "noun|verb|etc",
+      synonyms: [
+        "exercise",
+      ],
+      examples: [
+        {
+          type: "code-block",
+          code: "<code></code>",
+        },
+        {
+          type: "codepen",
+          link: "url here",
+        },
+      ],
+    },
+  ],
+  category: {
+    id: 1,
+    title: "Category",
+    slug: "category",
+  },
+  tags: [
+    {
+      id: 1,
+      title: "Tag[n]",
+      slug: "tag[n]",
+    },
+  ],
+};
 
 
 export default words;
